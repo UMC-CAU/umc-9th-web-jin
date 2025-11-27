51d41f19943ba8e8962fbd130f224c720682fbc3
jin0504 <heejinee0504@naver.com>
2025-11-27 16:53:18 +0900

    week9 mission1&2

---
+++ week9-3/src/constants/cartItems.ts
@@ -0,0 +1,36 @@
+const cartItems = [
+    {
+        id: 'recB6qcHPxb62YJ75',
+        title: 'Vancouver',
+        singer: '빅나티',
+        price: 25000,
+        img: 'https://www.akbobada.com/home/akbobada/archive/akbo/img/202206182335056.jpg',
+        amount: 1,
+    },
+    {
+    id: 'recD8abHPx2039A23',
+    title: 'Get A Guitar',
+    singer: 'RIIZE',
+    price: 18000,
+    img: 'https://i.namu.wiki/i/C3DYt1SXA7PjbnKEhfvTShkS33-7eAhF9zyPzSA2IjUCquO_3yM4VcHVzBGdEQKKt1NtyIDoFDFfjf6n6p-QqA.webp',
+    amount: 1,
+  },
+  {
+    id: 'recC901HPx0021Z88',
+    title: 'Smoothie',
+    singer: 'NCT Dream',
+    price: 18000,
+    img: 'https://i.namu.wiki/i/a7uuLUi0j-e-Xw5IftIvIC9fN0XkrbDjCdXNEWEeMcdnEenMzgRfmmOidhJqo_0Rzq7xb6rxqofq1A8LF6JTow.webp',
+    amount: 1,
+  },
+  {
+    id: 'recA444HPx9900L12',
+    title: 'Songbird',
+    singer: 'NCT WISH',
+    price: 18000,
+    img: 'https://i.namu.wiki/i/nriKiClto76epeLnxYNLFUyHPYYwhkqaWCL2Mzly47QilFGDzm8-oCMkx33ghm3EzAVUabXsmS5t1EqCroISJw.jpg',
+    amount: 1,
+  },
+]
+
+export default cartItems;
