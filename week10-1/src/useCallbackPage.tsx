89501647073d90a7b17af32beae90c52118a0faf
jin0504 <heejinee0504@naver.com>
2025-12-03 11:15:43 +0900

    week10실습1수정

---
+++ week10-1/src/useCallbackPage.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useState } from "react";
+import CountButton from "./components/CountButton";
+import TextInput from "./components/TextInput";
+
+export default function useCallbackPage() {
+    const [count, setCount] = useState<number>(0);
+    const [text, setText] = useState<string>("");
+
+    const handleIncreaseCount = useCallback((number: number) => {
+        setCount(count + number)
+    }, [count]);
+
+    const handleText = useCallback((text: string) => {
+        setText(text);
+    }, []);
+
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <CountButton onClick={handleIncreaseCount} />
+            <h2>Text</h2>
+            <div className="flex flex-col">
+                <span>Text: {text}</span>
+                <TextInput onChange={handleText} />
+            </div>
+        </div>
+    );
+}
