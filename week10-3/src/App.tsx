df06c181d3876961f9dfb4b8a433c45d66777339
jin0504 <heejinee0504@naver.com>
2025-12-04 12:47:57 +0900

    week10mission1

---
+++ week10-3/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+
+export default function App() {
+
+  console.log('App component rendering');
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
